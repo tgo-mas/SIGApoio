745933527f8a19928c1f788e6dab233eee26dbf2
MarlonHD <marlonsilva840@gmail.com>
2024-04-04 00:17:31 -0300

    add classe Usuario #41

--- proj_SIGApoio/app_SIGApoio/models.py
+++ proj_SIGApoio/app_SIGApoio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TipoUsuario(models.Model):
+    tipo = models.CharField(max_length=50, unique=True, primary_key=True)
+
+class Usuario(models.Model):
+    matricula = models.IntegerField(primary_key=True, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    nome = models.CharField(max_length=200)
+    tipo = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    #escala  = models.ForeignKey(Horario, on_delete=models.SET_NULL, default=None)
