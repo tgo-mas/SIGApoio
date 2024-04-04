5db492e0f32273045dc9c4704683a5a034b842aa
MarlonHD <marlonsilva840@gmail.com>
2024-04-04 00:17:58 -0300

    add classe Recurso #41

--- proj_SIGApoio/app_SIGApoio/models.py
+++ proj_SIGApoio/app_SIGApoio/models.py
@@ -10,3 +10,12 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=200)
     tipo = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
     #escala  = models.ForeignKey(Horario, on_delete=models.SET_NULL, default=None)
+
+class Recurso(models.Model):
+    
+    STATUS_CHOICE = ((True,"Disponível"),(False,"Indispovível"))
+    FUNCIONANDO_CHOICE = ((True,"Sim"),(False,"Não"))
+    
+    codigo = models.IntegerField(primary_key=True, unique=True)
+    status = models.BooleanField(choices=STATUS_CHOICE, blank=True, default=True,)
+    funcionando = models.BooleanField(choices=FUNCIONANDO_CHOICE, blank=True, default=True)
