7da06032030f490d91a2d101d7345dfe33253973
AndersonAzeved <andersonsilva14.2017@gmail.com>
2024-04-08 17:03:29 -0300

    alterações na isse #41

--- proj_SIGApoio/app_SIGApoio/models.py
+++ proj_SIGApoio/app_SIGApoio/models.py
@@ -1,33 +1,37 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class TipoUsuario(models.Model):
-#     tipo = models.CharField(max_length=50, unique=True, primary_key=True)
+# Create your models here.
+class TipoUsuario(models.Model):
+    tipo = models.CharField(max_length=50, unique=True, primary_key=True)
 
-# class Usuario(models.Model):
-#     matricula = models.IntegerField(primary_key=True, unique=True)
-#     email = models.EmailField(max_length=100, unique=True)
-#     nome = models.CharField(max_length=200)
-#     tipo = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
-#     #escala  = models.ForeignKey(Horario, on_delete=models.SET_NULL, default=None)
+class Usuario(models.Model):
+    matricula = models.IntegerField(primary_key=True, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    nome = models.CharField(max_length=200)
+    tipo = models.OneToOneField(TipoUsuario, on_delete=models.CASCADE)
+    #escala  = models.ForeignKey(Horario, on_delete=models.SET_NULL, default=None)
 
-# class TipoRecurso(models.Model):
-#     tipo = models.CharField(max_length=100)
+class TipoRecurso(models.Model):
+    tipo = models.CharField(max_length=100)
 
-# class Recurso(models.Model):
+class Recurso(models.Model):
     
-#     STATUS_CHOICE = ((True,"Disponível"),(False,"Indispovível"))
-#     FUNCIONANDO_CHOICE = ((True,"Sim"),(False,"Não"))
+    STATUS_CHOICE = ((True,"Disponível"),(False,"Indispovível"))
+    FUNCIONANDO_CHOICE = ((True,"Sim"),(False,"Não"))
     
-#     codigo = models.IntegerField(primary_key=True, unique=True)
-#     tipo = models.ForeignKey(TipoRecurso, on_delete=models.CASCADE)
-#     status = models.BooleanField(choices=STATUS_CHOICE, blank=True, default=True,)
-#     funcionando = models.BooleanField(choices=FUNCIONANDO_CHOICE, blank=True, default=True)
+    codigo = models.IntegerField(primary_key=True, unique=True)
+    tipo = models.ForeignKey(TipoRecurso, on_delete=models.CASCADE)
+    status = models.BooleanField(choices=STATUS_CHOICE, blank=True, default=True,)
+    funcionando = models.BooleanField(choices=FUNCIONANDO_CHOICE, blank=True, default=True)
 
-# class Emprestimo(models.Model):
-#     horaSaida = models.DateTimeField()
-#     horaEntrada = models.DateTimeField()
-#     id = models.IntegerField(primary_key=True, unique=True, serialize=True)
-#     idRecurso = models.ForeignKey(Recurso, on_delete=models.DO_NOTHING)
-#     matBolsista = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
-#     matUsuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+class Emprestimo(models.Model):
+    horaSaida = models.DateTimeField(auto_now=True)
+    horaEntrada = models.DateTimeField()
+    idRecurso = models.ForeignKey(Recurso, on_delete=models.DO_NOTHING)
+    matBolsista = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING) 
+    matUsuario = models.ForeignKey(Usuario, related_name='%(class)s_usuario', on_delete=models.DO_NOTHING, default='')
+
+
+class Sala(models.Model):
+    bloco = models.CharField(max_length=1)
+    
--- proj_SIGApoio/proj_SIGApoio/urls.py
+++ proj_SIGApoio/proj_SIGApoio/urls.py
@@ -1,8 +1,9 @@
 
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from app_SIGApoio import views
 
 urlpatterns = [
     path('',views.home,name = "home"),
+    path('admin/', admin.site.urls)
 ]
