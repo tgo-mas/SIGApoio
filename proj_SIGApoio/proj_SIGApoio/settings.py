450dc11d2f312f417505ab918f71c4d28cb49d5f
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2024-07-04 21:28:33 -0300

    update settings #68

--- proj_SIGApoio/proj_SIGApoio/settings.py
+++ proj_SIGApoio/proj_SIGApoio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ga2o34_axy#$rj7k&!1yadwrwfat3ou1c7@=f3q#=y)@euuoa0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
