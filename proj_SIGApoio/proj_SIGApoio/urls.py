ab452b1917afb1748f1b3031c46ffefcf22433e4
Rafael Garcia <garciarafael.2298@gmail.com>
2024-07-05 09:06:02 -0300

    Configuração do banco da página de cadastro de local #64

--- proj_SIGApoio/proj_SIGApoio/urls.py
+++ proj_SIGApoio/proj_SIGApoio/urls.py
@@ -5,7 +5,8 @@ from app_SIGApoio import views
 
 urlpatterns = [
     path('',views.home,name = "home"),
-    path('cad_page/',views.cad_page, name = "cad_page"),
+    path('cad_local/',views.cad_local, name = "cad_local"),
+    path('success_page/', views.success_page, name = "success_page"),
     path('admin/', admin.site.urls)
 ]
 
