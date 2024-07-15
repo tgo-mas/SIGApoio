964e55c607c9c86281c98a0e26f7e83ae8707aa9
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2024-07-15 10:01:08 -0300

    atualizações no corpo do projeto e alterações no nome de pastas

--- proj_SIGApoio/core/urls.py
+++ proj_SIGApoio/core/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from app_SIGApoio import views
+from app import views
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,6 +11,5 @@ urlpatterns = [
     path('cad_local/',views.cad_local, name = "cad_local"),
     path('success_page/', views.success_page, name = "success_page"),
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
-    path('', include('app_SIGApoio.urls'))
+    path('', include('app.urls'))
     ]
