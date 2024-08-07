fe0354addbe5b10e9c903ac3f28a874177796dc5
MarlonHD <marlonsilva840@gmail.com>
2024-08-07 20:24:09 -0300

    criação da função de detalhar reserva #82

--- proj_SIGApoio/app/templatetags/func_utils.py
+++ proj_SIGApoio/app/templatetags/func_utils.py
@@ -1,4 +1,6 @@
 from django import template
+from ..models import TipoRecurso, Recurso, Local, ReservaSemanal, ReservaMensal, ReservaDiaUnico, Usuario, Horario, TipoLocal, Chamado
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -10,4 +12,32 @@ def to_int(value):
 def to_str(value):
     return str(value)
 
+@register.filter(name="detalhar")
+def detalhar(reserva):
+    result = ""
+    if isinstance(reserva, ReservaDiaUnico):
+        result = """
+        <div>
+            <div>%s</div>
+            <div>%s</div>
+            <div>%s</div>
+            <div>%s</div>
+            <div>%s</div>
+            <div>%s</div>
+        </div> 
+        """ %(reserva.descricao, reserva.diaHoraInicio, reserva.diaHoraFim, reserva.local, reserva.matResponsavel, reserva.matSolicitante)
 
+    elif isinstance(reserva, ReservaSemanal):
+        chamados = Chamado.objects.filter(reserva=reserva)
+        result = f"""
+        <div>
+            <div>{reserva.descricao}</div>
+            <div>{reserva.horarios}</div>
+            <div>{reserva.local}</div>
+            <div>{reserva.matResponsavel}</div>
+            <div>{reserva.matSolicitante}</div>"""
+        for chamado in chamados:
+            result += f"<div>{chamado}</div>"
+        result += "</div>"
+    
+    return mark_safe(result)
